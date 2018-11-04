f07b1bf217f74076ab930fdeab4774370d0f276a
puppetmaster <puppet-master@ntnu.no>
2018-11-04 13:12:35 +0000

    Configuration files and manifest for dovecot and sql

---
+++ files/sql/virtual_aliases.sql
@@ -0,0 +1,4 @@
+INSERT IGNORE INTO `servermail`.`virtual_aliases`
+(`id`, `domain_id`, `source`, `destination`)
+VALUES
+('1', '1', 'alias@example.com', 'email1@example.com');
---
+++ files/sql/virtual_domains.sql
@@ -0,0 +1,5 @@
+INSERT IGNORE INTO `servermail`.`virtual_domains`
+(`id` ,`name`)
+VALUES
+('1', 'example.com'),
+('2', 'hostname.example.com');
