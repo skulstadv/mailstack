1ddac5ba1f5f9dfddb27f19300b59a2c6b68d1a4
puppetmaster <puppet-master@ntnu.no>
2018-11-05 02:46:46 +0000

    Fixed permissions for /var/mail/*/* to vmail:vmail like it should be and added postfix master.cf

--- manifests/dovecot.pp
+++ manifests/dovecot.pp
@@ -52,6 +52,20 @@ class mailstack::dovecot {
   }
 
 
+  # Change owner of /var/mail/* to vmail
+  exec { 'owner var/mail':
+    command => 'chown -R vmail:vmail /var/mail/*',
+    path    => $::path,
+  }
+
+
+  # Change owner of /var/mail/*/* to vmail
+  exec { 'owner var/mail':
+    command => 'chown -R vmail:vmail /var/mail/*/*',
+    path    => $::path,
+  }
+
+
   # Manage dovecot installation
   class { 'dovecot':
   }
