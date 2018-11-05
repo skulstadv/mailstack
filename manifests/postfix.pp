04d074bb082a037d43b5b9de29a4d364fde4bc4a
puppetmaster <puppet-master@ntnu.no>
2018-11-05 19:40:10 +0100

    Update postfix.pp
    code quality changes after applying puppet-lint style checks

--- manifests/postfix.pp
+++ manifests/postfix.pp
@@ -3,16 +3,16 @@ class mailstack::postfix {
 
 
   # Copy premade certificate *THIS MUST BE CHANGED BY USER*
-  file { "/etc/ssl/certs/dovecot.pem":
-    ensure   => 'present',
-    source   => "puppet:///modules/mailstack/postfix/ssl/certs/dovecot.pem",
+  file { '/etc/ssl/certs/dovecot.pem':
+    ensure => 'present',
+    source => 'puppet:///modules/mailstack/postfix/ssl/certs/dovecot.pem',
   }
 
 
   # Copy premade certificate *THIS MUST BE CHANGED BY USER*
-  file { "/etc/ssl/private/dovecot.pem":
-    ensure   => 'present',
-    source   => "puppet:///modules/mailstack/postfix/ssl/private/dovecot.pem",
+  file { '/etc/ssl/private/dovecot.pem':
+    ensure => 'present',
+    source => 'puppet:///modules/mailstack/postfix/ssl/private/dovecot.pem',
   }
 
 
@@ -24,22 +24,22 @@ class mailstack::postfix {
 
   # Manage postfix installation
   class { 'postfix':
-    config_file_source => "puppet:///modules/mailstack/postfix/main/main.cf",
+    config_file_source => 'puppet:///modules/mailstack/postfix/main/main.cf',
   }
 
 
   # Copy postfix config files
-  file { "/etc/postfix/":
-    ensure   => 'directory',
-    source   => "puppet:///modules/mailstack/postfix",
-    recurse  => 'remote',
+  file { '/etc/postfix/':
+    ensure  => 'directory',
+    source  => 'puppet:///modules/mailstack/postfix',
+    recurse => 'remote',
   }
 
 
 # These functions are handy for testing your configuration
 #  # Make sure postfix can find the domains
 #  exec { "postfinstalledix-domains":
-#    command => "postmap -q example.com mysql:/etc/postfix/mysql-virtual-mailbox-domains.cf",
+#    command => 'postmap -q example.com mysql:/etc/postfix/mysql-virtual-mailbox-domains.cf',
 #    path    => $::path,
 #    returns => '1',
 #  }
@@ -47,7 +47,7 @@ class mailstack::postfix {
 #
 #  # Make sure postfix can find our first email address
 #  exec { "postfix-mailbox-maps":
-#    command => "postmap -q email1@example.com mysql:/etc/postfix/mysql-virtual-mailbox-maps.cf",
+#    command => 'postmap -q email1@example.com mysql:/etc/postfix/mysql-virtual-mailbox-maps.cf',
 #    path    => $::path,
 #    returns => '1',
 #  }
@@ -55,10 +55,11 @@ class mailstack::postfix {
 #
 #  # Make sure postfix can find our aliases
 #  exec { "postfix-alias-maps":
-#    command => "postmap -q alias@example.com mysql:/etc/postfix/mysql-virtual-alias-maps.cf",
+#    command => 'postmap -q alias@example.com mysql:/etc/postfix/mysql-virtual-alias-maps.cf',
 #    path    => $::path,
 #    returns => 'email1@example.com',
 #  }
 
 
 }
+
